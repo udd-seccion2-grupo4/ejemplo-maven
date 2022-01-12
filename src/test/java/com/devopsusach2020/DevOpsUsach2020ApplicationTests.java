339b0d0b7535fdfde6f79f2e76629d8138199d70
ngamboa <ngamboa@insacom.cl>
2022-01-12 09:41:09 -0300

    fix qg

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.devopsusach2020;
 
+import com.devopsusach2020.rest.RestData;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
+	@Autowired
+	private RestData controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		Assertions.assertNotNull(controller, "controller no puede ser null");
 	}
 
 }
