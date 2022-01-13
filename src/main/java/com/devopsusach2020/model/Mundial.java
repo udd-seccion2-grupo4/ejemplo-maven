97575b3d1c02cfc378964d8dc136b2e971155ca6
ngamboa <ngamboa@insacom.cl>
2022-01-12 21:33:27 -0300

    ignoro carpeta relacionado a sq

--- src/main/java/com/devopsusach2020/model/Mundial.java
+++ src/main/java/com/devopsusach2020/model/Mundial.java
@@ -4,32 +4,34 @@ import java.io.Serializable;
 
 public class Mundial implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3908000903498620526L;
-	
+
 	private int totalconfirmed;
 	private int totaldeaths;
 	private int totalrecovered;
-	
+
 	public int getTotalConfirmed() {
 		return totalconfirmed;
 	}
+
 	public void setTotalConfirmed(int totalConfirmed) {
 		this.totalconfirmed = totalConfirmed;
 	}
+
 	public int getTotalDeaths() {
 		return totaldeaths;
 	}
+
 	public void setTotalDeaths(int totalDeaths) {
 		this.totaldeaths = totalDeaths;
 	}
+
 	public int getTotalRecovered() {
 		return totalrecovered;
 	}
+
 	public void setTotalRecovered(int totalRecovered) {
 		this.totalrecovered = totalRecovered;
-	}	
+	}
 
 }
